2f5d0a5a31282296c15d928a85086e7a4b603097
philander18 <philander.totalis@gmail.com>
2025-04-10 10:25:48 +0700

    menambah ps1

---
+++ copy_epicor.ps1
@@ -0,0 +1,11 @@
+﻿if (Test-Path C:\Epicor){
+    if (Test-Path C:\Epicor\ERP10-CR){
+        Remove-Item -Path C:\Epicor\ERP10-CR -Recurse -Force
+        }
+    if (Test-Path 'C:\Epicor\Shortcut ERP10-CR'){
+        Remove-Item -Path 'C:\Epicor\Shortcut ERP10-CR' -Recurse -Force
+        }
+    } else {
+    New-Item -Path 'C:\Epicor' -ItemType Directory
+    }
+xcopy "\\ftpcr\master$\Instalasi\1. Standar Medion\64 Bit\26. Epicor\Epicor" C:\Epicor /S /Y
